6f50be65844aa7b21e69288ce371211fbf870af3
HP <HP@DESKTOP-G4J7E8F>
2022-07-29 18:40:07 +0530

    Add project files.

---
+++ FileResultDemo/Controllers/FileDemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileResultDemo.Controllers
+{
+    public class FileDemoController : Controller
+    {
+        // GET: FileDemo
+        public FileResult Index()
+        {
+            return File("~/ContentsData/habbits.pdf","application/pdf");
+        }
+    }
+}
